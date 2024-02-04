1106fa66f34f27029d3a256c038ef618867297d0
Dexarc <sarenkumar86@gmail.com>
2024-02-05 03:11:14 +0530

    package28

--- app/index.js
+++ app/index.js
@@ -1,15 +1,35 @@
-// index.js
 const express = require('express');
-const loadDatabaseSecrets = require('./secrets.js'); // Assuming loadDatabaseSecrets is the correct function to call
+const loadDatabaseSecrets = require('./secrets.js');
 const mysql = require('mysql2/promise');
+
+const MAX_RETRIES = 3;
+
+async function executeQuery(pool) {
+  let retryCount = 0;
+
+  while (retryCount < MAX_RETRIES) {
+    try {
+      const [rows] = await pool.execute('SELECT * FROM entries');
+      return rows;
+    } catch (error) {
+      console.error('Error executing database query:', error);
+
+      retryCount++;
+      console.log(`Retrying... (Attempt ${retryCount})`);
+
+      // Add a delay before the next attempt (optional)
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  }
+
+  throw new Error('Retry limit reached. Unable to execute database query.');
+}
 
 async function startServer() {
   try {
     const secrets = await loadDatabaseSecrets();
-    // Verify that 'secrets' is an object with the 'database' property
     const { host, user, password, database } = secrets.database;
 
-    // Create a MySQL connection pool
     const pool = mysql.createPool({
       host,
       user,
@@ -24,21 +44,18 @@ async function startServer() {
     const app = express();
     const PORT = process.env.PORT || 8080;
 
-    // Middleware to parse JSON bodies
     app.use(express.json());
 
-    // Example REST API endpoint to retrieve data from the database
     app.get('/', async (req, res) => {
       try {
-        const [rows] = await pool.execute('SELECT * FROM entries');
+        const rows = await executeQuery(pool);
         res.json({ data: rows });
       } catch (error) {
-        console.error('Error executing database query:', error.message);
+        console.error('Error handling database query:', error.message);
         res.status(500).json({ error: 'Internal Server Error' });
       }
     });
 
-    // Start the server
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
